d4dc75260a60c184170d5314df56d49be02dcd73
felipeblobo <felipeblobo@gmail.com>
2021-05-19 14:08:55 -0300

    desenvolvendo Home

--- src/components/Spotlight/Spotlight.js
+++ src/components/Spotlight/Spotlight.js
@@ -1,7 +1,16 @@
-function Hero() {
-    return(
-        <h1>Hello, world</h1>
-    )
+import "./spotlight.css";
+
+function Spotlight() {
+  return (
+    <>
+      <section class="spotlight-section spotlight-section-one">
+        <h1 class="spotlight-title">Hello, world</h1>
+      </section>
+      <section class="spotlight-section spotlight-section-two">
+        <h1 class="spotlight-title">Hello, world</h1>
+      </section>
+    </>
+  );
 }
 
-export default Hero;
+export default Spotlight;
