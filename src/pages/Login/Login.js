55e556751cd48a3926cb5b9067a11e1a4aed67ae
felipeblobo <felipeblobo@gmail.com>
2021-05-28 22:03:44 -0300

    ajustes no pull

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,42 +1,12 @@
 import "./login.css";
-import { TextField, Button } from "@material-ui/core";
-
+import  LoginComponent  from "../../components/LoginComponent/LoginComponent";
 
 function Login() {
   return (
-    <>     
-      <div className='login-container'>
-        <div className="container-first">
-          <div className="first-column">
-            <h2 className="title"> Login</h2>
-            <form className="form">
-              <div className="form-group">
-                <TextField
-                  className="form-control"
-                  type="email"
-                  placeholder="Email"
-                />
-              </div>
-              <div className="form-group">
-                <TextField
-                  className="form-control"
-                  type="password"
-                  placeholder="Password"
-                />
-              </div>
-              {/* <button className="btn btn-secondary">Entrar</button> */}
-              <Button
-                style={{ marginTop: "20px" }}
-                variant="contained"
-                className="buttonLogin"
-                color="primary"
-              >
-                Entrar
-              </Button>
-            </form>
-          </div>
-        </div>
-      </div>
+    <>
+    <div className="container-first">
+      <LoginComponent/>
+    </div>
     </>
   );
 }
