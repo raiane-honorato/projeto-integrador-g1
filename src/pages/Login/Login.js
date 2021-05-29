d05a26e3e510dbca70f6464658563ac3301dc98d
Raiane Honorato <raianehonorato16@gmail.com>
2021-05-28 22:57:31 -0300

    forgot password setup

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,12 +2,19 @@ import "./login.css";
 import { TextField, Button } from "@material-ui/core";
 import { Link, NavLink } from "react-router-dom";
 import  LoginComponent  from "../../components/LoginComponent/LoginComponent";
+import ForgotComponent from "../../components/LoginComponent/ForgotComponent"
+import React, {Component, useEffect, useState} from 'react';
 
 function Login() {
+
+  const [changePass, setChangePass] = useState(false);  
+
+  console.log(!changePass)
   return (
     <>
     <div className="container-first">
-      <LoginComponent/>
+      {!changePass && <LoginComponent statePass = {changePass} setStatePass = {setChangePass}/>}
+      {changePass && <ForgotComponent statePass = {changePass} setStatePass = {setChangePass}/>}
     </div>
     </>
   );
