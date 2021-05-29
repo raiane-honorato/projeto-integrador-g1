651332a6bbf06547c74469cf5761857ca6f02a1d
Raiane Honorato <raianehonorato16@gmail.com>
2021-05-28 23:21:01 -0300

    felipe merge and set ups

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,6 +4,8 @@ import { Link, NavLink } from "react-router-dom";
 import  LoginComponent  from "../../components/LoginComponent/LoginComponent";
 import ForgotComponent from "../../components/LoginComponent/ForgotComponent"
 import React, {Component, useEffect, useState} from 'react';
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
 
 function Login() {
 
@@ -11,12 +13,14 @@ function Login() {
 
   console.log(!changePass)
   return (
-    <>
+    <div className="loginContainer">
+    <Navbar />
     <div className="container-first">
       {!changePass && <LoginComponent statePass = {changePass} setStatePass = {setChangePass}/>}
       {changePass && <ForgotComponent statePass = {changePass} setStatePass = {setChangePass}/>}
     </div>
-    </>
+    <Footer />
+    </div>
   );
 }
 
