a24892acc2137176133d8294e802b794f4c68d73
felipeblobo <felipeblobo@gmail.com>
2021-06-05 08:17:41 -0300

    nav refeita

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,50 +2,33 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 import miniLogo from "../../img/mini-logo-white.png";
 import SearchBar from "../SearchBar/SearchBar";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-
-function Navbar({navbarTransparent, changeBackground}) {
-
-  window.addEventListener('scroll', changeBackground)
+function Navbar({ navbarTransparent, changeBackground }) {
+  window.addEventListener("scroll", changeBackground);
 
   return (
     <>
-      <nav className="navbar" id={ navbarTransparent ? 'navbarTransparent' : '' }>
+      <nav className="navbar" id={navbarTransparent ? "navbarTransparent" : ""}>
         <div className="nav-container">
           <div>
-            <img
-              src={miniLogo}
-              className="nav-logo"
-              alt="logo-corrente-do-bem"
-            ></img>
-            <ul className="nav-btn-list">
-              <li>
-                <NavLink to="/" className="nav-btn" exact>
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/register_user" className="nav-btn">
-                  Cadastro
-                </NavLink>
-              </li>
-              <li className='login'>
-                <NavLink to="/login" className="nav-btn">
-                  Login
-                </NavLink>
-              </li>
-            </ul>
+            <NavLink to="/" className="nav-btn" exact>
+              <img
+                src={miniLogo}
+                className="nav-logo"
+                alt="logo-corrente-do-bem"
+              ></img>
+            </NavLink>
           </div>
           <SearchBar />
-        {/* <FontAwesomeIcon className="nav-responsive-menu"
-                icon={ faBars }
-                size="2x"
-                alt="Menu"
-
-              /> */}
+          <ul className="nav-btn-list">
+            <li className="login">
+              <NavLink to="/login" className="nav-btn" id="login">
+              Entrar
+                <span class="material-icons material-icons-outlined">login</span>
+                
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
