f8b88398f9803896ea0429a019d7f147d9a1b8ac
felipeblobo <felipeblobo@gmail.com>
2021-06-29 23:14:57 -0300

    responsividade da pg de home finalizada e parcialmente do usuario

--- src/components/UserData/UserData.js
+++ src/components/UserData/UserData.js
@@ -16,8 +16,7 @@ function UserData() {
   const [causes, setCauses] = useState();
   const [habilities, setHabilities] = useState();
 
-
-   //states of content edition
+  //states of content edition
   const [firstEditState, setFirstEditState] = useState(false);
   const [secondEditState, setSecondEditState] = useState(false);
   const [thirdEditState, setThirdEditState] = useState(false);
@@ -45,156 +44,163 @@ function UserData() {
       .catch((erro) => alert("Não foi possível obter os projetos do usuário."));
   }, [userId]);
 
-
   useEffect(() => {
     let requests =
       pageUser.causes &&
-      pageUser.causes.map( async (cause_id) => {
+      pageUser.causes.map(async (cause_id) => {
         const res = await fetch(`http://localhost:8000/cause/${cause_id}`);
         return await res.json();
       });
 
-      requests && Promise.all(requests)
-      .then((p) => setCauses(p))
-      .catch((err) => alert("Não foi possível obter as causas"));
+    requests &&
+      Promise.all(requests)
+        .then((p) => setCauses(p))
+        .catch((err) => alert("Não foi possível obter as causas"));
   }, [pageUser]);
-
 
   useEffect(() => {
     let requests2 =
       pageUser.habilities &&
       pageUser.habilities.map(async (hability_id) => {
-        const res = await fetch(`http://localhost:8000/hability/${hability_id}`);
+        const res = await fetch(
+          `http://localhost:8000/hability/${hability_id}`
+        );
         return await res.json();
       });
 
-      requests2 && Promise.all(requests2)
-      .then((p) => setHabilities(p))
-      .catch((err) => alert("Não foi possível obter as habilidades"));
+    requests2 &&
+      Promise.all(requests2)
+        .then((p) => setHabilities(p))
+        .catch((err) => alert("Não foi possível obter as habilidades"));
   }, [pageUser]);
 
   return (
     <>
-    <div className="userdata-container">
-      {user && (
-        <>
-          <Toaster />
-          <div className="profile-basic-information">
-            <img src={pageUser.img} alt="Foto do usuário" />
-            <h1>{pageUser.name}</h1>
-            <div className='causes-section'>
-               {causes && <span>Causas:</span>}
-              {causes && causes.map(cause => (
-                <span key={cause.id}>{cause.label}</span>
-              ))}
-            </div>
-
-            <div className='habilities-section'>
-              {habilities && <span>Habilidades:</span>}
-              {habilities && habilities.map(hability => (
-                <span key={hability.id}>{hability.label}</span>
-              ))}
-            </div>
-                     
-            {user.id === pageUser.id && (
-              <>
-              <div className="profileEdition">
-                <EditButton
-                  editClass="user-first-edit"
-                  setStatePass={setThirdEditState}
-                />
-                <span>Causas e Habilidades</span>
+      <div className="userdata-container">
+        {user && (
+          <>
+            <Toaster />
+            <div className="profile-basic-information">
+              <img src={pageUser.img} alt="Foto do usuário" />
+              <h1>{pageUser.name}</h1>
+              <div className="causes-section">
+                {causes && <span>Causas:</span>}
+                {causes &&
+                  causes.map((cause) => (
+                    <span key={cause.id}>{cause.label}</span>
+                  ))}
               </div>
-              <div className="profileEdition">
-              <EditButton
-                editClass="user-first-edit"
-                setStatePass={setSecondEditState}
-              />
-              <span>Inserir Endereço</span>
+
+              <div className="habilities-section">
+                {habilities && <span>Habilidades:</span>}
+                {habilities &&
+                  habilities.map((hability) => (
+                    <span key={hability.id}>{hability.label}</span>
+                  ))}
+              </div>
+
+              {user.id === pageUser.id && (
+                <>
+                  <div className="profileEdition">
+                    <EditButton
+                      editClass="user-first-edit"
+                      setStatePass={setThirdEditState}
+                    />
+                    <span>Causas e Habilidades</span>
+                  </div>
+                  <div className="profileEdition">
+                    <EditButton
+                      editClass="user-first-edit"
+                      setStatePass={setSecondEditState}
+                    />
+                    <span>Inserir Endereço</span>
+                  </div>
+                </>
+              )}
             </div>
-            </>
-            )}
-          </div>
-          <div className="profile-other-data">
-            <div className="personal-data">
-              <div className="first-edition-box">
-                <h2>Dados pessoais</h2>
-                {user.id === pageUser.id && (
-                  <EditButton
-                    editClass="user-first-edit"
-                    setStatePass={setFirstEditState}
-                  />
-                )}
+            <div className="profile-other-data">
+              <div className="personal-data">
+                <div className="first-edition-box">
+                  <h2>Dados pessoais</h2>
+                  {user.id === pageUser.id && (
+                    <EditButton
+                      editClass="user-first-edit"
+                      setStatePass={setFirstEditState}
+                    />
+                  )}
+                </div>
+                <div className="section-personal-data">
+                  <div className="first-section-personal-data">
+                    <p>
+                      <span>Data de Nascimento:</span> {pageUser.birth_date}
+                    </p>
+                    <p>
+                      <span>Telefone:</span> {pageUser.phone}
+                    </p>
+                    <p>
+                      <span>Email:</span> {pageUser.email}
+                    </p>
+                  </div>
+                  <div className="second-section-personal-data">
+                    <p>
+                      <span>Rua:</span> {pageUser.street}
+                    </p>
+                    <p>
+                      <span>Número:</span> {pageUser.address_number}
+                    </p>
+                    <p>
+                      <span>Bairro:</span> {pageUser.bairro}
+                    </p>
+                    <p>
+                      <span>Cidade:</span> {pageUser.city}
+                    </p>
+                    <p>
+                      <span>Estado:</span> {pageUser.state}
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className='section-personal-data'>
-
-              <div className="first-section-personal-data">
-                <p>
-                  <span>Data de Nascimento:</span> {pageUser.birth_date}
-                </p>
-                <p>
-                  <span>Telefone:</span> {pageUser.phone}
-                </p>
-                <p>
-                  <span>Email:</span> {pageUser.email}
-                </p>
-              </div>
-              <div className="second-section-personal-data">
-                <p>
-                  <span>Rua:</span> {pageUser.street}
-                </p>
-                <p>
-                  <span>Número:</span> {pageUser.address_number}
-                </p>
-                <p>
-                  <span>Bairro:</span> {pageUser.bairro}
-                </p>
-                <p>
-                  <span>Cidade:</span> {pageUser.city}
-                </p>
-                <p>
-                  <span>Estado:</span> {pageUser.state}
-                </p>
-              </div>
+              <div className="projects-data">
+                {user.id === pageUser.id && <h2>Projetos</h2>}
+                {subscriptions &&
+                  user.id === pageUser.id &&
+                  subscriptions.map((subscription) => (
+                    <UserSubscriptionCart
+                      key={subscription.id}
+                      subscription={subscription}
+                    />
+                  ))}
               </div>
             </div>
-            <div className="projects-data">
-              {user.id === pageUser.id && <h2>Projetos</h2>}
-              {subscriptions &&
-                user.id === pageUser.id &&
-                subscriptions.map((subscription) => (
-                  <UserSubscriptionCart key={subscription.id} subscription={subscription} />
-                ))}
-            </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
 
-      {(firstEditState || secondEditState || thirdEditState) && (
-        <ProfileEditionSection
-          firstEditState={firstEditState}
-          secondEditState={secondEditState}
-          thirdEditState={thirdEditState}
-          setStatePass={
-            firstEditState
-              ? setFirstEditState
-              : secondEditState
-              ? setSecondEditState
-              : thirdEditState
-              ? setThirdEditState
-              : ""
-          }
-          setPageUser={setPageUser}
-          pageUser={pageUser}
-        />
-      )}
-
-
-    </div>
+        {(firstEditState || secondEditState || thirdEditState) && (
+          <ProfileEditionSection
+            firstEditState={firstEditState}
+            secondEditState={secondEditState}
+            thirdEditState={thirdEditState}
+            setStatePass={
+              firstEditState
+                ? setFirstEditState
+                : secondEditState
+                ? setSecondEditState
+                : thirdEditState
+                ? setThirdEditState
+                : ""
+            }
+            setPageUser={setPageUser}
+            pageUser={pageUser}
+          />
+        )}
+      </div>
 
       <div
-        className={`user-overlay ${firstEditState || secondEditState || thirdEditState ? "user-set-vis" : ""
-          }`}
+        className={`user-overlay ${
+          firstEditState || secondEditState || thirdEditState
+            ? "user-set-vis"
+            : ""
+        }`}
         onClick={() => {
           setFirstEditState(false);
           setSecondEditState(false);
@@ -203,7 +209,7 @@ function UserData() {
       >
         {" "}
       </div>
-</>
+    </>
   );
 }
 
