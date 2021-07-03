bdc3b1f8a507994467858e7a7aeff115f3468efa
Raiane Honorato <raianehonorato16@gmail.com>
2021-07-03 19:37:43 -0300

    filtering projects based on project status feature

--- src/components/ManageProjects/ManageProjectsList/ManageProjectsList.js
+++ src/components/ManageProjects/ManageProjectsList/ManageProjectsList.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef } from "react";
 import { AuthContext } from "../../../context/auth";
 
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -18,6 +18,25 @@ function ManageProjectsList() {
     const [newProject, setNewProject] = useState(false);
     const [q, setQ] = useState('');
     const [filterStatus, setFilterStatus] = useState(false);
+    const [filterState, setFilterState] = useState({'opened':false,'closed':false})
+    const [filterProject, setFilterProject] = useState({'opened':false,'closed':false})
+
+    //dealing with outside click to close the component
+    let windowRef = useRef();
+
+    useEffect(() => {
+        let handler = (event) => {
+                if(!windowRef.current.contains(event.target)){
+                  setFilterStatus(false)
+                  setFilterState(filterProject)
+                }
+                }
+        filterStatus && document.addEventListener("mousedown", handler);
+
+        return () => {
+            filterStatus && document.removeEventListener("mousedown", handler)
+        }
+    },[filterStatus])
 
     useEffect(() => {
       
@@ -59,8 +78,49 @@ function ManageProjectsList() {
         .catch((erro) =>
           alert("Não foi possível obter dados dos projetos.")
         )
+
+        q && cleanFilter()
         
       }, [q])
+
+      let handleFilterChange = (event) => {
+        setFilterState({...filterState, [event.target.name] : event.target.checked })
+      }
+
+      let cleanFilter = () => {
+        setFilterState({'opened':false,'closed':false})
+        setFilterProject({'opened':false,'closed':false})
+      }
+
+      let updateProjects = () => {
+        setFilterProject(filterState)
+        setQ("")
+        if (filterState.opened == filterState.closed) {
+          fetch(`http://localhost:8000/projects/?institution_id=${user.institution_id}`)
+          .then((res) => res.json())
+          .then((res) => {
+            setProjects(res);
+          })
+          .catch((erro) =>
+            alert("Não foi possível obter dados dos projetos.")
+          )
+        } else {
+          fetch(`http://localhost:8000/projects/?institution_id=${user.institution_id}${filterState.opened ? '&status=1': filterState.closed ? '&status=2':''}`)
+          .then((res) => res.json())
+          .then((res) => {
+            setProjects(res);
+          })
+          .catch((erro) =>
+            alert("Não foi possível obter dados dos projetos.")
+          )
+        }
+
+        setFilterStatus(false)
+      }
+
+      // useEffect(() => {
+      //   if (filterProject.opened == false & filterProject.closed == false){updateProjects()}
+      // },[filterProject])
 
   return (
     <>
@@ -78,11 +138,15 @@ function ManageProjectsList() {
                   className = "manage-projects-search-input" 
                   type = "text" 
                   placeholder = "buscar projeto"
+                  value = {q}
                   onChange = {(event) => {setQ(event.target.value)}}
                    />
                 </div>  
-                <div className="manage-projects-filter-field-container">
-                  <div className="manage-projects-filter-field" onClick = {()=> setFilterStatus(!filterStatus)}>
+                <div className="manage-projects-filter-field-container" ref = {windowRef} >
+                  <div className="manage-projects-filter-field" onClick = {()=> {
+                    setFilterStatus(!filterStatus);
+                    setFilterState(filterProject)
+                    }}>
                     <p>Todas as vagas</p>
                     <FontAwesomeIcon className = "manage-projects-icon" icon = {faChevronDown} />
                   </div>
@@ -90,14 +154,34 @@ function ManageProjectsList() {
                   {filterStatus && 
                   
                   <div  className = "manage-projects-filter-dropdown">
-                    <ul className="manage-projects-filter-dropdown-list">
-                        <li className="manage-projects-filter-dropdown-list-item">
-                            Aceitar inscrição
-                        </li>
-                        <li className="manage-project-subscription-change-status-dropdown-btn"  >
-                            Recusar inscrição
-                        </li>
-                    </ul>
+                    <label className = "manage-projects-filter-option">
+                        <input 
+                          className = "manage-projects-filter-checkbox" 
+                          type="checkbox" 
+                          name="opened"
+                          checked = {filterState.opened}
+                          onChange = {handleFilterChange}
+                          >
+                          
+                        </input>
+                        <span className = "manage-projects-filter-text">Abertas</span>
+                    </label>
+                    <label className = "manage-projects-filter-option">
+                    <input 
+                          className = "manage-projects-filter-checkbox" 
+                          type="checkbox" 
+                          name="closed"
+                          checked = {filterState.closed}
+                          onChange = {handleFilterChange}
+                          >
+                          
+                        </input>                          
+                        <span className = "manage-projects-filter-text">Encerradas</span>
+                    </label>
+                    <div className = "manage-projects-filter-btn-container">
+                      <button className = "manage-projects-filter-btn" onClick = {cleanFilter}>Limpar</button>
+                      <button className = "manage-projects-filter-btn" id="manage-projects-apply-btn" onClick = {updateProjects}>Aplicar</button>
+                    </div>
                   </div>
                   }
 
